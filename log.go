914a303c03a02fa234f3a786f3ebc3ce790f6916
jwijenbergh <jeroenwijenbergh@protonmail.com>
2024-01-17 13:13:49 +0100

    Log: Change Log in Logger interface from interface{} to string

--- log.go
+++ log.go
@@ -1,13 +1,13 @@
 package eduoauth
 
 type Logger interface {
-	Log(_ ...interface{})
+	Log(_ string)
 	Logf(_ string, _ ...interface{})
 }
 
 type nullLogger struct{}
 
-func (l nullLogger) Log(_ ...interface{})            {}
+func (l nullLogger) Log(_ string)            {}
 func (l nullLogger) Logf(_ string, _ ...interface{}) {}
 
 var log Logger = nullLogger{}
